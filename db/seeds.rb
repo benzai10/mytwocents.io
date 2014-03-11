8b7a59f4227c72dbaa6072bd5b04b2e4503a5890
David Antman <semihelix@gmail.com>
2014-03-11 17:18:58 +0900

    Updated seed msg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Post.create(content: "First post!")
+Post.create(content: "Sometimes, you just want to get rid of your thoughts or share something with anyone. You don’t care to whom or where. You don’t want to think about who is following you. You don’t care about if people like it or not. You don’t want to be commented or judged. On mytwocents.io you can just give your two cents. Anything (unless it’s not against the terms of service), anytime, anywhere. You don’t have to think about who is following you or who do you have in your friends list.
+
+The world is your audience and they might ignore your two cents completely (it’s just your two cents, remember?) or, and that might be motivating: You inspire someone, somewhere in this world and you contributed something valuable. Keep this in mind when you’re giving your two cents!
+
+Basically, it’s like a single, central Twitter account for the whole world! Let’s get crazy and have some fun!")
