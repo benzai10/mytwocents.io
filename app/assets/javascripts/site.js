880cf11a9e97e3912ce5051005fbfb9d57aa326c
David Antman <semihelix@gmail.com>
2014-03-12 11:33:02 +0900

    Fixed refresh time

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,7 +3,7 @@ $(function() {
     setInterval(function(){
       $('.counter').load('/posts/counter');
       $('.new_posts').load('/posts/new_posts');
-    }, 115000);
+    }, 5000);
 
     $('.feed').click(function (event) {
       $('.posts').load('/posts/feed');
